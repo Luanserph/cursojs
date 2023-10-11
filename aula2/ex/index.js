2345eeb896cb799601b0840090b34f48db532ac9
Luanserph <darkserphps4@gmail.com>
2023-10-11 12:07:11 -0300

    aspas, crase e comentario no console.log

---
+++ aula2/ex/index.js
@@ -0,0 +1,5 @@
+console.log("Luan Silva"); // Strings = texto
+console.log('Luan Silva'); 
+console.log(`Luan Silva`); // Crase = Template Strings
+
+console.log("Luan Silva,", 29, "anos,", "Data de Nascimento:",10,"/",2,"/",1994);
