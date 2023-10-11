ab117e56bccc52222b842198c7221be7634a777e
Luanserph <darkserphps4@gmail.com>
2023-10-11 17:24:36 -0300

    Navegador vs Node (HTML + JavaScript)

---
+++ aula5/js/index.js
@@ -0,0 +1,4 @@
+//comentario, tudo aqui será de certa forma exclusiva do javascript, tenha em mente que essa posição do script no final do body é uma boa pratica, pois nao irá retardar o carregamento de sua pagina.
+
+console.log("Teste do comando.");
+console.log("Comando só será exibido no console do navegador.")
