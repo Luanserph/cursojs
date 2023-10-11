e17d1c175a8eccf2ef9dbf0f80afd5ce34a5f5a4
Luanserph <darkserphps4@gmail.com>
2023-10-11 12:00:07 -0300

    exemplificando console log com string e numeral

---
+++ aula3/ex/index.js
@@ -0,0 +1,5 @@
+console.log("EXEMPLO 1:");
+console.log("Meu nome é 'Luan'. Estou aprendendo JavaScript às",(10),"da manhã.");
+
+console.log("EXEMPLO 2:");
+console.log("Meu nome é Luan. Estou aprendendo JavaScript às 10 da manhã.");
