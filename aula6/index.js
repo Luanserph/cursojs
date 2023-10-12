2d5e69caf49c35a8941c39dbedc559427cf1e851
LuanSerph <darkserphps4@gmail.com>
2023-10-11 21:29:22 -0300

    Variaveis 'let'

---
+++ aula6/index.js
@@ -0,0 +1,9 @@
+let nome = "Luan"; //String, let representa uma variavel que é nome e o valor dela é joão.
+let nome2 = "Jéssica";
+let filho = "Pedro";
+let filho2 = "Alice";
+
+console.log(nome,"conheceu", nome2,"em 2011.");
+console.log("Em 2016,",nome,"casou-se com",nome2);  
+console.log(nome2,"teve um filho de",nome,"em 2017, chamado,",filho);
+console.log("Mais tarde, em 2022",nome2,"teve uma menina, conhecida como,",filho2);
