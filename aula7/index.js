63cb43c42329068cedb006b2d8753641eb683d08
Luanserph <darkserphps4@gmail.com>
2023-10-16 17:28:24 -0300

    constantes com const e typeof

---
+++ aula7/index.js
@@ -0,0 +1,29 @@
+//UTILIZAÇÃO DE UMA CONTANTE 
+//NÃO UTILIZA VAR, UTILIZE CONST
+//NÃO PODE MODIFICAR O VALOR DE UMA CONSTANTE
+
+const clienteNome = 'Luan';
+console.log(clienteNome);
+
+console.log('\n_*_*_*_* Operações básicas de aritmética _*_*_*_*')
+
+console.log('\nIdentificação das contantes das constantes ')
+const primeiroNumero = 20;
+const segundoNumero = 10;
+console.log(primeiroNumero)
+console.log(segundoNumero)
+
+console.log('\nSoma das constantes: ')
+console.log(primeiroNumero, '+', segundoNumero)
+
+const resultado = primeiroNumero + segundoNumero;
+
+console.log('\nResultado das constantes: ')
+console.log(resultado)
+
+console.log('\nDobro do resultado')
+const duplicarResultado = resultado * 2;
+console.log(duplicarResultado)
+
+console.log(typeof primeiroNumero, '<----tipo da variavel: primeiroNumero')
+
