e3cb8af81afa67622d71ba251c5a88806c29a593
Luanserph <darkserphps4@gmail.com>
2023-10-16 15:04:34 -0300

    adição sobre camelCase

--- aula6/index.js
+++ aula6/index.js
@@ -7,3 +7,14 @@ console.log(nome,"conheceu", nome2,"em 2011.");
 console.log("Em 2016,",nome,"casou-se com",nome2);  
 console.log(nome2,"teve um filho de",nome,"em 2017, chamado,",filho);
 console.log("Mais tarde, em 2022",nome2,"teve uma menina, conhecida como,",filho2);
+
+//Manter as boas praticas 
+//procurar fazer uso de camelCase
+//Sem espaços ou traços 
+//variaveis com nomes significativos
+//variaveis com Case-sensitive (Tipografia faz total diferença, maiuscula e minuscula)
+
+console.log('\ncamelCase TEST')
+let nomeCliente = ('Luan Silva')
+
+console.log(nomeCliente)
