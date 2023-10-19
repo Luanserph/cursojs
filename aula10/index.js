06a004edb9e5d14e8d0568b761d09ee581350cd8
Luanserph <darkserphps4@gmail.com>
2023-10-19 17:39:24 -0300

    Operadores aritméticos, de atribuição, incremento e decremento

---
+++ aula10/index.js
@@ -0,0 +1,31 @@
+//Operadores aritméticos, de atribuição e incremento
+
+
+let num1 = 10;
+let num2 = 5;
+let num3 = 2;
+
+const soma = num1 + num2 + num3;
+const sub = num1 - num2 - num3;
+const div = num1 / num2;
+const multi = num1 * num2;
+const pot = num1 ** num2;
+console.log('=============================================================');
+console.log('====== Adição das variaveis ======')
+console.log(`${soma}`);
+console.log('===== Subtração das variaveis =====')
+console.log(`${sub}`);
+console.log('===== Multiplicação das variaveis =====')
+console.log(`${multi}`);
+console.log('===== Divisão das variaveis =====')
+console.log(`${div}`);
+console.log('===== Potenciação das variaveis =====')
+console.log(`${pot}`);
+console.log('===== PRÉ Incrementação das variaveis =====')
+console.log(`${++num1}`);
+console.log('===== PÓS Incrementação das variaveis =====')
+console.log(`${num1++}`,`${num1}`);
+console.log('===== PRÉ Redução das variaveis =====')
+console.log(`${--num1}`);
+console.log('===== PÓS Redução das variaveis =====')
+console.log(`${num1--}`,`${num1}`);
